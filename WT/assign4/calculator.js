4863e1862d9a2933b46dfbface45e5670da84855
Ansh <74647041+ofcansh@users.noreply.github.com>
2025-03-17 12:57:26 +0530

    Add files via upload

--- WT/assign4/calculator.js
+++ WT/assign4/calculator.js
@@ -1,26 +1,26 @@
-const display = document.getElementById('display');
+const display = document.getElementById("display");
 
 function appendToDisplay(value) {
-    if (value === 'sqrt') {
-        display.value += 'Math.sqrt(';
-    } else {
-        display.value += value;
-    }
+  if (value === "sqrt") {
+    display.value += "Math.sqrt(";
+  } else {
+    display.value += value;
+  }
 }
 
 function clearDisplay() {
-    display.value = '';
+  display.value = "";
 }
 
 function backspace() {
-    display.value = display.value.slice(0, -1);
+  display.value = display.value.slice(0, -1);
 }
 
 function calculateResult() {
-    try {
-        let expression = display.value.replace(/\^/g, '**');
-        display.value = eval(expression);
-    } catch (error) {
-        display.value = 'Error';
-    }
+  try {
+    let expression = display.value.replace(/\^/g, "**");
+    display.value = eval(expression);
+  } catch (error) {
+    alert("Error\nEnter correct expression");
+  }
 }
